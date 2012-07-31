d86feb17c3cfa7ee839a0385a254e87562470d5e
Seyaa <barbot.max@gmail.com>
2012-07-31 17:10:37 +0200

    Signed-off-by: Seyaa <barbot.max@gmail.com>

--- src/ihm/Fenetre_principale.java
+++ src/ihm/Fenetre_principale.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package project.novelis;
+package ihm;
 
 import classe.Chargement;
 import classe.Fonctions;
